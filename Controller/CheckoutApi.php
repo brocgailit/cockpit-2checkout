8035fe390163a8b0a22cbb6d628d8393b20cae49
Broc Gailit <brocgailit@gmail.com>
2019-12-10 17:19:44 -0800

    test just data return

--- Controller/CheckoutApi.php
+++ Controller/CheckoutApi.php
@@ -38,6 +38,7 @@ class CheckoutApi extends Controller {
 	public function auth() {
 		if($this->req_is('post')) {
 			$data = json_decode(file_get_contents('php://input'), true);
+			return $data;
 			return $this->checkout->post('rs/authService', $data);
 		};
 	}
